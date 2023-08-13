30826d8c4c121608386e3f7c9acb130c8e726944
Noel Maher <danoely@gmail.com>
2023-08-13 11:13:21 +0100

    Comp update

--- src/pages/hackathon/competition.js
+++ src/pages/hackathon/competition.js
@@ -11,6 +11,7 @@ const Competition = ({ user }) => {
         title="The Full Stack Hackathon"
         description="Announcing our first hackathon for you or your team to build an awesome project, get exposure and win awesome prizes."
         keywords="developer, social network, developers, software engineering, full stack, software engineering network, tech community, tech companies, best tech companies, developer portfolio, developer network, professional network, professional community"
+        openGraphImage={'/assets/landing/hackathon/og.webp'}
       />
       <Layout user={user} fullWidth={true}>
         <Page />
